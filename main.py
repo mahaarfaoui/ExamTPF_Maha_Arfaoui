6c08bf78f2d24f2bbdb5c9b111cb96ed7ac5e377
maha <arfaouimaahaa@gmail.com>
2025-03-10 20:47:07 +0100

    fusion de feature dans seul commit

--- main.py
+++ main.py
@@ -9,5 +9,14 @@ som = somme(my_list)
 print('La somme est =', som)
 
 my_list = [1, 5, 7]
+
 print('La somme est =', sum(my_list))
->>>>>>> feature_list
+
+if my_list:
+    print('La somme est :', sum(my_list))
+    print('Le max est :', max(my_list))
+    print('Le min est :', min(my_list))
+else:
+    print("Liste vide")
+
+
